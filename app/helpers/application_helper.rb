457c698ae12809977a628173f167521686f97ff7
shimbiro <shimbirosharon@gmail.com>
2016-11-29 10:45:43 +0300

    js,users,micropost

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = 'Friends For Friends'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
